bd3167f74fecc011f30653eaf8d19b288dc261b4
Fedor Baart <fedor.baart@deltares.nl>
2014-02-14 21:39:00 +0100

    forgot comma,

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         'Topic :: System :: Distributed Computing',
 
         # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
