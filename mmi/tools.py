fe8ba1d5a26ead05b02ec50c4b9fb4c5c071b261
Fedor Baart <fedor.baart@deltares.nl>
2014-06-27 23:16:08 +0200

    get/set_var through req/pull socket

--- mmi/tools.py
+++ mmi/tools.py
@@ -41,32 +41,6 @@ logger.setLevel(logging.DEBUG)
 
 ioloop.install()
 
-OUTPUTVARS=[]
-INITVARS=[]
-def parse_args():
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument(
-        '-n', '--interval', dest='interval',
-        help='publishing results every [n] tiemsteps',
-        type=int,
-        default=1)
-    argparser.add_argument(
-        '-o', '--outputvariables', dest='outputvariables',
-        metavar='O',
-        nargs='*',
-        help='variables to be published',
-        default=OUTPUTVARS)
-    argparser.add_argument(
-        '-g', '--global', dest='globalvariables',
-        metavar='G',
-        nargs='*',
-        help='variables that can be send back to a reply (not changed during run)',
-        default=INITVARS)
-    argparser.add_argument(
-        'ini',
-        help='model configuration file')
-    return argparser.parse_args()
-
 
 # see or an in memory numpy message:
 # http://zeromq.github.io/pyzmq/serialization.html
@@ -84,16 +58,23 @@ def process_incoming(model, poller, rep, pull, data):
         for i in range(n):
             A, metadata = recv_array(sock)
             logger.info("got metadata: %s", metadata)
-            if metadata.get("action") == "send grid":
-                logger.info("sending grid")
+            if "get_var" in metadata:
+                name = metadata["get_var"]
+                logger.info("sending variable %s", name)
                 # temporary implementation
-                sock.send_pyobj(data)
-            elif "action" in metadata:
-                logger.info("found action applying update")
+                var = model.get_var(name)
+                metadata = {'name': name}
+                send_array(rep, var, metadata=metadata)
+                # assert socket is req socket
+            elif "set_var" in metadata:
+                name = metadata["set_var"]
+                logger.info("setting variable %s", name)
+                arr = model.set_var(name, A)
+                # todo assert socket is pull socket
+            elif "operator" in metadata:
                 # TODO: support same operators as MPI_ops here....,
                 # TODO: reduce before apply
-                action = metadata['action']
-                arr = model.get_var(metadata['name'])
+                # TODO: assert pull socket
                 S = tuple(slice(*x) for x in action['slice'])
                 print(repr(arr[S]))
                 if action['operator'] == 'setitem':
