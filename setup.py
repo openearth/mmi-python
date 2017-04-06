6223c2a05bd58cd9d950033e32cf6d650ef054ca
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2017-04-06 17:24:55 +0200

    Back to development: 0.1.9.3+nens

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import codecs
 # get py3 compatible open
 from io import open
 
-version = '0.1.9.2+nens'
+version = '0.1.9.3+nens.dev0'
 
 # Get the long description from the relevant file
 with open('DESCRIPTION.rst', encoding='utf-8') as f:
