1d724db19f215760f2fa2346e9512f38dd2fa6ef
Fedor Baart <fedor.baart@deltares.nl>
2018-03-30 11:18:36 +0200

    subscribe

--- mmi/mmi_client.py
+++ mmi/mmi_client.py
@@ -1,3 +1,4 @@
+import urllib.parse
 import zmq
 
 from mmi import send_array, recv_array
@@ -13,15 +14,40 @@ class MMIClient(IBmi):
         # Open ZeroMQ socket
         context = zmq.Context()
 
-        self.socket = context.socket(zmq.REQ)
-        self.socket.connect(zmq_address)
+        self.sockets = {}
+        
+        host, port = urllib.parse.splitport(zmq_address)
 
-        self.poll = zmq.Poller()
-        self.poll.register(self.socket, zmq.POLLIN)
+
+        ports = {
+            zmq.REQ: int(port),
+            zmq.PUSH: int(port) + 1,
+            zmq.SUB: int(port) + 2
+        }
+
+        for socket, port in ports.items():
+            self.sockets[socket] = context.socket(socket)
+            self.sockets[socket].connect(
+                ":".join([
+                    host,
+                    str(port)
+                ])
+            )
+
+
+        self.pollers = {}
+        for socket in [zmq.REQ, zmq.PUSH, zmq.SUB]:
+            poller = zmq.Poller()
+            self.pollers[socket] = poller
+            self.pollers[socket].register(self.sockets[socket], zmq.POLLIN)
 
         self.poll_timeout = poll_timeout
         self.zmq_flags = zmq_flags
 
+        self.socket = self.sockets[zmq.REQ]
+        self.poll = self.pollers[zmq.REQ]
+
+        
     def _close_sockets(self):
         self.socket.setsockopt(zmq.LINGER, 0)
         self.socket.close()
@@ -285,3 +311,7 @@ class MMIClient(IBmi):
             flags=self.zmq_flags)
 
         return metadata[method]
+
+    def subscribe(self, topic=b''):
+        """subscribe to the SUB socket, to listen for incomming variables"""
+        self.sockets[zmq.SUB].setsockopt(zmq.SUBSCRIBE, topic)
