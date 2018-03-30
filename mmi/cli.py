72343bafac0fba0a9c83edfaaec808649b06122a
Fedor Baart <fedor.baart@deltares.nl>
2018-03-30 14:12:16 +0200

    docs

--- mmi/cli.py
+++ mmi/cli.py
@@ -14,8 +14,52 @@ def cli(args=None):
 
 @cli.command()
 @click.argument('engine')
-@click.argument('configfile')
-def runner(engine, configfile):
+@click.argument(
+    'configfile',
+    type=click.Path(exists=True)
+)
+@click.option(
+    '--output',
+    '-o',
+    'output_vars',
+    multiple=True,
+    help='output variables, will be broadcasted each <interval> timestep'
+)
+@click.option(
+    '--global',
+    '-g',
+    'global_vars',
+    multiple=True,
+    help='global variables, will be send on request'
+)
+@click.option(
+    '--interval',
+    type=float,
+    help='publish results every <interval> timesteps'
+)
+@click.option(
+    '--pause',
+    is_flag=True,
+    help='start in paused mode, send update messages to progress'
+)
+@click.option(
+    '--mpi',
+    type=click.Choice(['root', 'all']),
+    help="communicate with mpi nodes using one of the methods: root (communicate with rank 0), all (one socket per rank)"
+)
+@click.option('--track', help='server to subscribe to for tracking')
+@click.option(
+    '--port',
+    type=int,
+    help='"random" or integer base port, port is computed as req/rep = port + rank*3 + 0, push/pull = port + rank*3 + 1, pub/sub = port + rank*3 + 2 [default: random]'
+)
+@click.option(
+    '--bmi-class',
+    'bmi_class',
+    default='bmi.wrapper.BMIWrapper',
+    help='the full name of a python class that implements bmi'
+)
+def runner(engine, configfile, output_vars, global_vars, interval, pause, mpi, track, port, bmi_class):
     """
     mmi-runner <engine> <configfile> [-o <outputvar>...] [-g <globalvar>...] [--interval <interval>] [--disable-logger] [--pause] [--mpi <method>] [--track <server>] [--port <port>] [--bmi-class]
     """
